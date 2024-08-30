b2a030feddf80714ea7ca2823bf0ac393519a432
Sachin Kumar panchal <123099867+Panchal2003@users.noreply.github.com>
2024-08-30 19:12:16 +0530

    Add files via upload

---
+++ webinar/Webinar/webinar.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const weekdaysBtn = document.getElementById("weekdays-btn");
+    const weekendBtn = document.getElementById("weekend-btn");
+
+    weekdaysBtn.addEventListener("click", function() {
+        weekdaysBtn.classList.add("active");
+        weekendBtn.classList.remove("active");
+    });
+
+    weekendBtn.addEventListener("click", function() {
+        weekendBtn.classList.add("active");
+        weekdaysBtn.classList.remove("active");
+    });
+});
+
+function toggleFaq(index) {
+    var answers = document.querySelectorAll('.faq-answer');
+    var questions = document.querySelectorAll('.faq-question');
+
+    if (answers[index].style.display === 'block') {
+        answers[index].style.display = 'none';
+        questions[index].classList.remove('active');
+    } else {
+        for (var i = 0; i < answers.length; i++) {
+            answers[i].style.display = 'none';
+            questions[i].classList.remove('active');
+        }
+        answers[index].style.display = 'block';
+        questions[index].classList.add('active');
+    }
+}
